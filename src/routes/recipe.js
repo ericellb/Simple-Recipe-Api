91841bb3b90ebe6785e448b8a3abe2bf1234d72b
shmeegie <ericellb@gmail.com>
2019-08-01 21:26:29 -0400

    Added status codes

--- src/routes/recipe.js
+++ src/routes/recipe.js
@@ -6,27 +6,27 @@ router.get('/recipes', (req, res) => {
   // Return recipes of specific cuisineType
   if (req.query.cuisineType) {
     RecipeModel.find({ type: req.query.cuisineType }, (err, docs) => {
-      res.send(docs);
+      res.status(200).send(docs);
     });
   }
   // Return recipes of specific foodType
   else if (req.query.foodType) {
     RecipeModel.find({ type: req.query.foodType }, (err, docs) => {
-      res.send(docs);
+      res.status(200).send(docs);
     });
   }
   // Return 1 recipe with given ID
   else if (req.query.id) {
     console.log(req.query.id);
     RecipeModel.find({ _id: req.query.id }, (err, docs) => {
-      res.send(docs);
+      res.status(200).send(docs);
     })
   }
   // Return all recipes only the title, description and type
   // used for frontend autocomplete dictionary
   else if (req.query.dictionary === '1') {
     RecipeModel.find({}, 'title description type', (err, docs) => {
-      res.send(docs);
+      res.status(200).send(docs);
     });
   }
   else {
